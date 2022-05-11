863c16e8c7447d3c1bdfa733b444e0307d89141b
felipeparisantunes <felipe@parisantunes.com.br>
2022-05-10 23:20:27 -0300

    Atualizando arquivos

--- libpytools/spam/enviador_de_email.py
+++ libpytools/spam/enviador_de_email.py
@@ -1,3 +1,9 @@
 class Enviador:
     def Enviar(self, remetente, destinatario, assunto, mensagem):
+        if "@" not in remetente:
+            raise EmailInvalido(f'O Email do remetente é invalido: {remetente}')
         return remetente
+
+
+class EmailInvalido(Exception):
+    pass
--- libpytools/tests/tests_spam/test_enviador_de_email.py
+++ libpytools/tests/tests_spam/test_enviador_de_email.py
@@ -1,6 +1,6 @@
 import pytest
 
-from libpytools.spam.enviador_de_email import Enviador
+from libpytools.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_email():
@@ -8,17 +8,30 @@ def test_criar_enviador_email():
     assert enviador is not None
 
 @pytest.mark.parametrize(
-    'destinatario',
+    'remetente',
     ['felipe@parisantunes.com.br', 'f3lipe_antunes@hotmail.com']
 )
-def test_remetente(destinatario):
+def test_remetente(remetente):
     enviador=Enviador()
-    destinatarios = ['felipe@parisantunes.com.br', 'f3lipe_antunes@hotmail.com']
     resultado = enviador.Enviar(
-        destinatario,
+        remetente,
         'felipe.p.antunes@hotmail.com',
         'Assunto',
         'Corpo do Email é a mensagem'
     )
-    assert destinatario in resultado
+    assert remetente in resultado
 
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'f3lipe_antunes']
+)
+def test_remetente_invalido(remetente):
+    enviador=Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.Enviar(
+            remetente,
+            'felipe.p.antunes@hotmail.com',
+            'Assunto',
+            'Corpo do Email é a mensagem'
+            )
